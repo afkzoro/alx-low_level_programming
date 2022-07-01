b9c2affc05bfc63d5c46bc266e5dfb49203d0f8f
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-07-01 08:38:07 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int	main(void)
+{
+	int Char;
+
+	for (Char = 97; Char <= 122; Char++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
