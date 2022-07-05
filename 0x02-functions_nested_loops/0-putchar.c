c0559623d3b910198dcd23be8a76c28b210c9706
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-07-05 04:04:04 -0700

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+
+	printf("%s", c);
+	printf("\n");
+	return (0);
+}
