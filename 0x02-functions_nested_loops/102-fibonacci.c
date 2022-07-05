3528da4876ebc016bc65a3dfa77c8b3e0c4a0cfd
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-07-05 09:35:54 -0700

    omo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Finish
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%1d", j);
+		else if (i == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %1d", k);
+		}
+
+		++i;
+	}
+
+	printf("\n");
+	return (0);
+}
