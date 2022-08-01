1712557782653dc655418c81e2a7853bedbbc372
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-01 10:35:17 +0100

    commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * free_dog - Frees dynamic memory allocated to dog
- *
- * @d: Pointer to the dog struct
- * Return: Void
- */
+	* free_dog - Frees dynamic memory allocated to dog
+	*
+	* @d: Pointer to the dog struct
+	* Return: Void
+	*/
 void free_dog(dog_t *d)
 {
- if (d)
- {
-  free((*d).name);
-  free((*d).owner);
-  free(d);
- }
+	if (d)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
 }
