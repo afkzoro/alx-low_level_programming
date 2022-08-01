235850603336abc469b857d0a8994a5cb0c26a02
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-01 12:53:05 +0100

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+#define NAME __FILE__
+
 /**
 	* main - Prints name of file
 	* Return: Always 0(success)
 	*/
 int main()
 {
-	printf("%s\n", __FILE__);
+	printf("%s\n", NAME);
 	return (0);
 }
