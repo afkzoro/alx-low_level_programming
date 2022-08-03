6e2ee195448c1cf557b54015d15054af67a52f32
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-03 13:46:20 +0100

    commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,42 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-		* main - Main function
-		* @argc: Number of arguments
-		* @argv: Argument vectors
-		*
-		* Return: Always 0(Success)
-		*/
+	* main - Run the program
+	* @argc: Count of args given
+	* @argv: Array of arg strings
+	*
+	* Return: 0 on success, print Error and exit with 98 if incorrect
+	* number of args, exit with 99 if operator given is incorrect
+	*/
 int main(int argc, char *argv[])
 {
-	int (*oper)(int, int);
+	int (*func)(int, int);
+	int num1, num2, res;
 
-	if (argc != 4)
+	if (argc == 4)
+	{
+		if (argv[2][1] != '\0')
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		func = get_op_func(argv[2]);
+		if (func == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+		res = func(num1, num2);
+		printf("%d\n", res);
+		return (0);
+	}
+	else
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	oper = get_op_func(argv[2]);
-
-	if (!oper)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	printf("%d\n", oper(atoi(argv[1]), atoi(argv[3])));
-	return (0);
 }
