8a24f4bbdf3de85bb1d15ada42b08ab2fbffe589
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-04 19:51:12 +0100

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,6 +4,7 @@
 /**
  * sum_them_all - return the sum of all its parameters
  * @n: number of extra params
+	* @...: Other parameters
  * Return: total sum
  */
 int sum_them_all(const unsigned int n, ...)
@@ -11,6 +12,9 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 	int sum;
+
+	if (n == 0)
+	return (0);
 
 	va_start(list, n);
 	for (i = 0, sum = 0; i < n; i++)
