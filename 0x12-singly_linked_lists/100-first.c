c702735157331b95e385bc76d6ab94207722eea4
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-11 13:36:57 +0100

    fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,10 @@
 void firstExec (void) __attribute__ ((constructor));
 
 /**
-	* first - Code executed before the main function
+	* firstExec - Code executed before the main function
 	* Return: void
 	*/
-void firstExec(void)
+void firstExec (void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
