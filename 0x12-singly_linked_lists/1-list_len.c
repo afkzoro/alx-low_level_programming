0335253da2683bc21719541f4b123f0b858d3766
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-11 12:34:01 +0100

    commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * list_len - Prinyts length of linked list
+ *
+ * @h: pointer to linked list
+ * Return: Returns the count
+ */
 size_t list_len(const list_t *h)
 {
 	size_t num = 0;
