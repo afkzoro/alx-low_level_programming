75284a067614b57199ea35d8ca444bb893f8dac9
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-11 13:41:28 +0100

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include "stdio.h"
 
-void firstExec (void) __attribute__ ((constructor));
+void firstExec(void) __attribute__ ((constructor));
 
 /**
 	* firstExec - Code executed before the main function
 	* Return: void
 	*/
-void firstExec (void)
+void firstExec(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
