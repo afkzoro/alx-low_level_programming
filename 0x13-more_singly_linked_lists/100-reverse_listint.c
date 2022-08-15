1c9304f202ec3411f4a2fc2a705d93676b433afa
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-15 20:09:54 +0100

    commit

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,13 +11,18 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *prev = NULL;
 	listint_t *next = NULL;
 
-	while (*head != NULL)
+	if (head)
 	{
-		next = (*head)->next;
-		(*head)->next = prev;
-
-		prev = *head;
-		*head = next;
+		while (*head != NULL)
+		{
+			next = (*head)->next;
+			(*head)->next = prev;
+			prev = *head;
+			*head = next;
+		}
+		head = prev;
+		return (*head);
 	}
-	*head = prev;
+
+	return (NULL);
 }
