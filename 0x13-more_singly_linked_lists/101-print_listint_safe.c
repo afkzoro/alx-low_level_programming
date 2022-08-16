e6c26a9e0da809fb97cc1da68aa7659105efb479
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-16 17:06:10 +0100

    commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,20 +1,20 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * loop_list_len - Finds loop by using two pointers
+	* loop_list_len - Finds loop by using two pointers
 	* if these pointers meet there's a loop in the linked list
- *
- * @head: Pointer to head of linked list
- * Return: Always 0 (Success)
- */
+	*
+	* @head: Pointer to head of linked list
+	* Return: Always 0 (Success)
+	*/
 size_t loop_list_len(const listint_t *head)
 {
+	/* Floyd’s Cycle-Finding Algorithm */
 	const listint_t *slow_p, *fast_p;
 	size_t count = 1;
 
-	if (head == NULL || head->next == NULL);
-	return (0);
+	if (head == NULL || head->next == NULL)
+		return (0);
 
 	slow_p = head->next;
 	fast_p = (head->next)->next;
@@ -42,23 +42,22 @@ size_t loop_list_len(const listint_t *head)
 		}
 
 		slow_p = slow_p->next;
-	 fast_p = (fast_p->next)->next;
+		fast_p = (fast_p->next)->next;
 	}
 
 	return (0);
 }
 
 /**
- * print_listint_safe - Prints the number of nodes in a linked list
+	* print_listint_safe - Prints the number of nodes in a linked list
 	* (loop involved lists too) and their addresses
- *
- * @head: Pointer to head of singly linked list
- * Return: NUmber of nodes in the list
- */
+	*
+	* @head: Pointer to head of singly linked list
+	* Return: NUmber of nodes in the list
+	*/
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count, index;
-	index = 0;
+	size_t count, index = 0;
 
 	count = loop_list_len(head);
 
@@ -83,52 +82,4 @@ size_t print_listint_safe(const listint_t *head)
 	}
 
 	return (count);
-}
-
-int main(void)
-{
-    listint_t *head;
-    listint_t *head2;
-    listint_t *node;
-
-    head2 = NULL;
-    add_nodeint(&head2, 0);
-    add_nodeint(&head2, 1);
-    add_nodeint(&head2, 2);
-    add_nodeint(&head2, 3);
-    add_nodeint(&head2, 4);
-    add_nodeint(&head2, 98);
-    add_nodeint(&head2, 402);
-    add_nodeint(&head2, 1024);
-    print_listint_safe(head2);
-    head = NULL;
-    node = add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    node->next = add_nodeint(&head, 98);
-    add_nodeint(&head, 402);
-    add_nodeint(&head, 1024);
-    print_listint_safe(head);
-    return (0);
-}
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new_node;
-
-	if (head != NULL)
-	{
-		new_node = (listint_t *)malloc(sizeof(listint_t));
-		if (new_node == NULL)
-		return (NULL);
-
-		(*new_node).n = n;
-		(*new_node).next = *head;
-		*head = new_node;
-
-		return (new_node);
-	}
-
-	return (NULL);
 }
