7476cf3990eab98d7ae018893e2beaa0512c1d91
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-16 09:14:48 +0100

    FIxed betty and loop mistakes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position(index)
- *
- * @head: Pointer to head of singly linked list
- * @idx: The index of the sigly linked list(Starts at 0)
- * @n: Data passed to the new node added at index
- * Return: returns a new node added at a given position or NULL if fails
- */
+	* insert_nodeint_at_index - Inserts a new node at a given position(index)
+	*
+	* @head: Pointer to head of singly linked list
+	* @idx: The index of the sigly linked list(Starts at 0)
+	* @n: Data passed to the new node added at index
+	* Return: returns a new node added at a given position or NULL if fails
+	*/
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
- listint_t *current, *new_node;
+	listint_t *current, *new_node;
 	unsigned int tally = 1;
 
 	if (head)
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			if (idx > tally)
 			return (NULL);
 		}
-	 else
+		else
 		{
 			new_node->next = *head;
 			*head = new_node;
