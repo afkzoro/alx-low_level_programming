c9b8933b8b8e6af5aa66969091a456963ed6194d
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-16 17:48:16 +0100

    commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,6 +35,7 @@ size_t print_listint_safe(const listint_t *head);
 size_t loop_list_len(const listint_t *head);
 size_t loop_listint_len(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
 
