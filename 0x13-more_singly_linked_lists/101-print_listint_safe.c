726bdfeadc0c9b376123244d7924de9ac16774be
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-16 16:25:09 +0100

    commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,11 +13,11 @@ size_t loop_list_len(const listint_t *head)
 	const listint_t *slow_p, *fast_p;
 	size_t count = 1;
 
-	if (head == NULL || head->next);
+	if (head == NULL || head->next == NULL);
 	return (0);
 
 	slow_p = head->next;
-	fast_p = (*head).next->next;
+	fast_p = (head->next)->next;
 
 	while (fast_p)
 	{
@@ -64,25 +64,71 @@ size_t print_listint_safe(const listint_t *head)
 
 	if (count == 0)
 	{
-		while(head != NULL)
+		for (; head != NULL; count++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
-			count++;
 		}
 	}
 
 	else
 	{
-		while (index < count)
+		for (index = 0; index < count; index++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
-			index++;
 		}
 
 		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
 	return (count);
+}
+
+int main(void)
+{
+    listint_t *head;
+    listint_t *head2;
+    listint_t *node;
+
+    head2 = NULL;
+    add_nodeint(&head2, 0);
+    add_nodeint(&head2, 1);
+    add_nodeint(&head2, 2);
+    add_nodeint(&head2, 3);
+    add_nodeint(&head2, 4);
+    add_nodeint(&head2, 98);
+    add_nodeint(&head2, 402);
+    add_nodeint(&head2, 1024);
+    print_listint_safe(head2);
+    head = NULL;
+    node = add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    node->next = add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint_safe(head);
+    return (0);
+}
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	if (head != NULL)
+	{
+		new_node = (listint_t *)malloc(sizeof(listint_t));
+		if (new_node == NULL)
+		return (NULL);
+
+		(*new_node).n = n;
+		(*new_node).next = *head;
+		*head = new_node;
+
+		return (new_node);
+	}
+
+	return (NULL);
 }
