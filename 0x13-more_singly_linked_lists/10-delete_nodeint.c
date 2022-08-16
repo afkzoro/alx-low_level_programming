48c3d87b7100dd5bba083da9ba35b2738c13a9e7
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-16 12:51:08 +0100

    commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
     return (1);
    }
 
-   for (count = 0; temp != NULL && count < index - 1; count++)
+   for (count = 1; temp != NULL && count < index - 1; count++)
    {
     temp = temp->next;
    }
