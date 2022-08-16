a631c859d2a92dd835a6df2f24c965a74f4d075f
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-16 08:32:31 +0100

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	return (new_node);
  }
 
- for(; tally != --idx; tally++)
+ for(; tally != idx - 1; tally++)
  {
 	current = (*current).next;
 	if (current == NULL)
