695615aec325945934a11dbd25b50168c5988de0
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-16 08:19:21 +0100

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,2 +1,37 @@
 #include "lists.h"
 
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+ listint_t *current, *new_node;
+ current = *head;
+ unsigned int tally = 0;
+
+ if (head == NULL)
+ return (NULL);
+ if (current == NULL && idx != 0)
+ return (NULL);
+
+ new_node = (listint_t *)malloc(sizeof(listint_t));
+ if (new_node == NULL)
+ return (NULL);
+ (*new_node).n = n;
+ if (idx == 0)
+ {
+	(*new_node).next = *head;
+	*head = new_node;
+	return (new_node);
+ }
+
+ for(; tally != --idx; tally++)
+ {
+	current = (*current).next;
+	if (current == NULL)
+	{
+		free (new_node);
+		return (NULL);
+	}
+ }
+ new_node->next = current->next;
+ current->next = new_node;
+ return (new_node);
+}
