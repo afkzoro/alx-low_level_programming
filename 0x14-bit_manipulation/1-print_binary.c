e9b2246b69d378690fffbeb6ac1df70c149c05db
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-18 13:14:29 +0100

    putchar and bit fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,17 +2,6 @@
 #include <unistd.h>
 
 /**
-	* _putchar - writes character c to stdout
-	* @c: character
-	* Return: on success 1
-	*
-	*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
 	* recursiveDiv - Performs recursive division
 	*
 	* @n: integer to convert to binary representation
@@ -24,7 +13,7 @@ void recursiveDiv(unsigned long int n)
 	if (n < 1)
 	return;
 
-	recursiveDiv(n / 2);
+	recursiveDiv(n >> 2);
 
 	if (n & 1)
 	_putchar('1');
