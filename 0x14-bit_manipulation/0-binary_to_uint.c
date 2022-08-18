49d5bc6ccb46b0dd0d72f2e7adfb5a5e413a510a
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-18 13:25:00 +0100

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,22 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int base = 0, len = 0, i = 0;
 
-	int c;
+	unsigned int c;
 
 	if (b == NULL)
 	return (0);
 
 	len = _strlen(b);
 
-	for (; c = len-- ; base++)
+	while (c = len--)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		return (0);
 
 		if (b[len] == '1')
 		i += 1 << base;
+
+		base++;
 	}
 
 	return (i);
