2a45429fc4bd9869fcb62812a2618d3b33c3a6ef
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-18 13:28:11 +0100

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,14 +10,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int base = 0, len = 0, i = 0;
 
-	unsigned int c;
-
 	if (b == NULL)
 	return (0);
 
 	len = _strlen(b);
 
-	while (c = len--)
+	while (len--)
 	{
 		if (b[len] != 48 && b[len] != 49)
 		return (0);
