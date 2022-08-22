1a0bcf557fa876db891999382d844c27dbfbe8db
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-22 12:38:36 +0100

    return fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,18 +11,18 @@ int create_file(const char *filename, char *text_content)
 {
 	int file_d;
 
-	if (!filename)
+	if (filename == NULL)
 	return (-1);
 
 	file_d = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_d == -1)
 	return (-1);
 
-	if (text_content)
+	if (text_content != NULL)
 	write(file_d, text_content, _strlen(text_content));
 
 	close(file_d);
-	return (-1);
+	return (1);
 }
 
 /**
