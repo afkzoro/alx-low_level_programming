12e433d95a32e9edf1beaf12d79402fc897c33cf
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-22 11:27:29 +0100

    append changes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * @brief
+ *
+ * @param filename
+ * @param text_content
+ * @return int
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int file_d;
