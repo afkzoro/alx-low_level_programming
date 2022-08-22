c7f4d5c62f01977eb9dad4983593078511746ce9
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-08-22 12:36:03 +0100

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,14 +11,14 @@ int create_file(const char *filename, char *text_content)
 {
 	int file_d;
 
-	if (filename == NULL)
+	if (!filename)
 	return (-1);
 
 	file_d = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_d == -1)
 	return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	write(file_d, text_content, _strlen(text_content));
 
 	close(file_d);
