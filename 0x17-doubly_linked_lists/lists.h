f169f9d50121ae0a7b3159e9223166544444b393
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-29 16:15:34 +0100

    commit

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,4 +27,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 dlistint_t *create_node(unsigned int n, void *next, void *prev);
-int delete_first_dnode(dlistint_t **head)
+int delete_first_dnode(dlistint_t **head);
+
