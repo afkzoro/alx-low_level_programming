c23513ecbb40a6706cefca7354f9598a2273d92a
Samuel Theophilus <samueltheophilus019@gmail.com>
2022-09-29 15:55:48 +0100

    commit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,13 +2,13 @@
 #include "lists.h"
 
 /**
-		* add_dnodeint_end - Adds a new node at the end
-		* of a doubly linked list
-		* @head: The head of the doubly linked list
-		* @n: The number to place in the new node
-		*
-		* Return: The new head of the doubly linked list
-		*/
+  * add_dnodeint_end - Adds a new node at the end
+  * of a doubly linked list
+  * @head: The head of the doubly linked list
+  * @n: The number to place in the new node
+  *
+  * Return: The new head of the doubly linked list
+  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *current = NULL, *new_node = NULL;
@@ -22,16 +22,16 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		current = *head;
 		while (current->next != NULL)
-			current = (*current).next;
+			current = current->next;
 
-		(*new_node).next = NULL;
-		(*new_node).prev = current;
-		(*current).next = new_node;
+		new_node->next = NULL;
+		new_node->prev = current;
+		current->next = new_node;
 		return (new_node);
 	}
 
-	(*new_node).next = *head;
-	(*new_node).prev = NULL;
+	new_node->next = *head;
+	new_node->prev = NULL;
 	*head = new_node;
 	return (*head);
 }
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current_node = head;
+	int add = 0;
+
+	if (head)
+	{
+		while (current_node != NULL)
+		{
+			add += (*current_node).n;
+			current_node = current_node->next;
+		}
+	}
+
+	return (add);
+}
